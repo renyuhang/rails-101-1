aeaf591207fe5fc288dd361b8d761d0a61717e6c
renyuhang <renyuhang126@126.com>
2017-04-05 21:13:59 +0800

    move form to partial

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -25,8 +25,7 @@ if @group.save
    end
  end
 
- redirect_to groups_path
-    end
+
 
     def update
         @group = Group.find(params[:id])
@@ -48,4 +47,5 @@ private
 
 def group_params
 params.require(:group).permit(:title, :description)
+end
 end
